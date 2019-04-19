0ab172cfbfb76951f222ae071d813ce94df3f228
mf <mf@metis.home.computing>
2019-04-19 01:02:17 -0300

    TODO: redis

--- bot.rb
+++ bot.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require_relative 'dankie'
 require_relative 'version'
 Dir[File.dirname(__FILE__) + '/plugins/*'].each { |file| require_relative file } # esta cosa carga todos los plugins del bot
@@ -21,7 +22,9 @@ plugins = Dir[File.dirname(__FILE__) + '/plugins/*.rb'].map { |file| file.split(
 bot.listen do |message|
     
     # TODO: validar blacklist acá (crearla antes)
-    next if message.is_a?(Telegram::Bot::Types::Message) && message.edit_date
+  next if dankie.blacklist.includes?(message&.from&.id)
+  
+  next if message.is_a?(Telegram::Bot::Types::Message) && message.edit_date
 
     # acá ocurre la magia
     plugins.each do |plugin|
