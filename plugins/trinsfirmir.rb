36f03dc4ca0228d8c6c5d853a921a7615bc93e5c
galerazo <caleidoscopioperonista@gmail.com>
2019-04-29 21:40:49 -0300

    Cambio mensaje trinsfirmir

--- plugins/trinsfirmir.rb
+++ plugins/trinsfirmir.rb
@@ -5,7 +5,7 @@ class Dankie
 
     def trinsfirmir(msg)
         unless (text = msg&.reply_to_message&.text || msg&.reply_to_message&.caption)
-            text = "Respondele a algo, #{TROESMAS.sample}."
+            text = "Respondele a algo que tenga texto, #{TROESMAS.sample}."
             @tg.send_message(chat_id: msg.chat.id, text: text)
             return
         end
