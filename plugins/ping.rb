efc9e8388bfab675097e64abcd6da52b161e4d18
Lucas F. Ovalle Olivares <luke.ovalle@gmail.com>
2019-05-03 01:43:03 -0300

    Ping acomodado un poco mas

--- plugins/ping.rb
+++ plugins/ping.rb
@@ -11,8 +11,8 @@ class Dankie
         time3 = Time.new
 
         text = "pong\n"
-        text << "telegram: `#{format('%.3f', time2.to_r - time1.to_r)}s`\n"
-        text << "base de datos: `#{format('%.3f', time3.to_r - time2.to_r)}s`"
+        text << format("%s: `%.3fs`\n", 'tg', time2.to_r - time1.to_r)
+        text << format("%s: `%.3fs`\n", 'bbdd', time3.to_r - time2.to_r)
         @tg.edit_message_text(chat_id: enviado.chat.id, parse_mode: 'markdown',
                               message_id: enviado.message_id, text: text)
         @logger.info text.tr("\n", "\t")
