e8f64ccf6622304809346b946ca8bf8779672563
galerazo <caleidoscopioperonista@gmail.com>
2019-06-14 12:37:49 -0300

    Le paso el sidacop

--- plugins/pole.rb
+++ plugins/pole.rb
@@ -64,26 +64,26 @@ class Dankie
         poles = @redis.zrevrange("pole:#{enviado.chat.id}",
                                  0, -1, with_scores: true)
         digits = poles.first[1].to_i.digits.count
-        
+
         tamaño = 0
         poles.each do |val|
             texto << "\n<code>#{format("%#{digits}d", val[1].to_i)} </code>"
             texto << get_username_link(enviado.chat.id, val[0])
-            
+
             if tamaño + texto.length > 4096
-            	tamaño = texto.length
-            	enviado = @tg.send_message(chat_id: msg.chat.id, text: texto,
-                                   		   parse_mode: 'html',
-                        	               disable_web_page_preview: true,
-                            	           disable_notification: true)
+                tamaño = texto.length
+                enviado = @tg.send_message(chat_id: msg.chat.id, text: texto,
+                                           parse_mode: 'html',
+                                           disable_web_page_preview: true,
+                                           disable_notification: true)
             else
-            	tamaño += texto.length
-            	@tg.edit_message_text(chat_id: enviado.chat.id, text: texto,
-                	                  parse_mode: 'html',
-                    	              message_id: enviado.message_id,
-                        	          disable_web_page_preview: true,
-                            	      disable_notification: true)
-        	end
+                tamaño += texto.length
+                @tg.edit_message_text(chat_id: enviado.chat.id, text: texto,
+                                      parse_mode: 'html',
+                                      message_id: enviado.message_id,
+                                      disable_web_page_preview: true,
+                                      disable_notification: true)
+            end
         end
     end
 end
