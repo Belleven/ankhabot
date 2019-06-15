c183f62e67584efda7381f3c2df0b378a2ed7b7a
Lucas F. Ovalle Olivares <luke.ovalle@gmail.com>
2019-06-15 16:28:00 -0300

    Pole.rb escrito en correcto castellano

--- plugins/pole.rb
+++ plugins/pole.rb
@@ -3,7 +3,7 @@ require 'concurrent-ruby'
 # Extension de dankie para manejar las poles
 class Dankie
     add_handler MessageHandler.new(:pole)
-    add_handler CommandHandler.new(:nisman, :send_pole_ranking,
+    add_handler CommandHandler.new(:nisman, :enviar_ranking_pole,
                                    description: 'Muestra el ranking de Nisman')
 
     # TODO: Ponerle algún flag de solo test a este comando
@@ -13,34 +13,34 @@ class Dankie
         id = msg.reply_to_message ? msg.reply_to_message.from.id : msg.from.id
         mensaje = msg.reply_to_message || msg
 
-        name = mensaje.from.first_name.empty? ? msg.from.id.to_s : html_parser(mensaje.from.first_name)
+        nombre = mensaje.from.first_name.empty? ? msg.from.id.to_s : html_parser(mensaje.from.first_name)
 
         @redis.zincrby("pole:#{msg.chat.id}", 1, id)
-        @logger.info("#{name} hizo la nisman en #{msg.chat.id}")
+        @logger.info("#{nombre} hizo la nisman en #{msg.chat.id}")
         @tg.send_message(chat_id: msg.chat.id, parse_mode: 'html',
                          reply_to_message_id: msg.message_id,
-                         text: "<b>#{name}</b> hizo la Nisman")
+                         text: "<b>#{nombre}</b> hizo la Nisman")
     end
 
     def pole(msg)
         return if @redis.exists("pole:#{msg.chat.id}:done")
 
-        now = Time.now
-        next_pole = @tz.utc_to_local(Time.new(now.year, now.month, now.day + 1))
-        next_pole = next_pole.to_i
-        msg_time = @tz.utc_to_local(Time.at(msg.date)).to_i
-        @redis.setex("pole:#{msg.chat.id}:done", next_pole - msg_time, 'ok')
+        ahora = Time.now
+        próx_pole = @tz.utc_to_local(Time.new(ahora.year, ahora.month, ahora.day + 1))
+        próx_pole = próx_pole.to_i
+        fecha_msg = @tz.utc_to_local(Time.at(msg.date)).to_i
+        @redis.setex("pole:#{msg.chat.id}:done", próx_pole - fecha_msg, 'ok')
         @redis.zincrby("pole:#{msg.chat.id}", 1, msg.from.id)
 
-        name = msg.from.first_name.empty? ? "ay no c (#{msg.from.id})" : html_parser(msg.from.first_name)
+        nombre = msg.from.first_name.empty? ? "ay no c (#{msg.from.id})" : html_parser(msg.from.first_name)
 
-        @logger.info("#{name} hizo la nisman en #{msg.chat.id}")
+        @logger.info("#{nombre} hizo la nisman en #{msg.chat.id}")
         @tg.send_message(chat_id: msg.chat.id, parse_mode: 'html',
                          reply_to_message_id: msg.message_id,
-                         text: "<b>#{name}</b> hizo la Nisman")
+                         text: "<b>#{nombre}</b> hizo la Nisman")
     end
 
-    def send_pole_ranking(msg)
+    def enviar_ranking_pole(msg)
         texto = '<b>Ranking de Nisman</b>'
         enviado = @tg.send_message(chat_id: msg.chat.id,
                                    parse_mode: 'html',
@@ -50,30 +50,31 @@ class Dankie
         # en vez de esto debería tener otra lista de plugins pesados que
         # trabajen en un hilo aparte
         fork do
-            edit_pole_ranking(enviado, texto)
+            editar_ranking_pole(enviado, texto)
         end
     end
 
-    def edit_pole_ranking(enviado, texto)
+    def editar_ranking_pole(enviado, texto)
         # Tomo las poles de las bases de datos y seteo los espacios para dígitos
         poles = @redis.zrevrange("pole:#{enviado.chat.id}", 0, -1, with_scores: true)
-        digits = poles.first[1].to_i.digits.count
+        dígitos = poles.first[1].to_i.digits.count
 
         # Tomo el total de poles y lo agrego al título
-        nismanes = dame_nismanes(poles)
-        texto << ' (' + nismanes + ")\n"
+        texto << " (#{ calcular_total_poles(poles) })\n"
 
         # Tomo otras variables que luego usaré
         chat_id = enviado.chat.id
-        indice = 0
+        índice = 0
 
-        poles.each do |val|
+        poles.each do |pole|
             # Armo la línea y el cargando... si es que no es la última línea
-            linea = "\n<code>#{format("%#{digits}d", val[1].to_i)}</code> " + get_username_link(enviado.chat.id, val[0])
-            cargando = indice == poles.length - 1 ? '' : "\n<i>cargando...</i>"
+            línea = "\n<code>#{format("%#{dígitos}d", pole[1].to_i)}</code> "
+            línea << get_username_link(enviado.chat.id, pole[0])
+
+            cargando = índice == poles.length - 1 ? '' : "\n<i>cargando...</i>"
 
             # Si el mensaje se pasa de los 4096 caracteres, mando uno nuevo
-            if texto.length + linea.length + cargando.length > 4096
+            if texto.length + línea.length + cargando.length > 4096
 
                 # Primero borro el "cargando" del mensaje anterior
                 @tg.edit_message_text(chat_id: chat_id, text: texto,
@@ -83,7 +84,7 @@ class Dankie
                                       disable_notification: true)
 
                 # Después mando el nuevo mensaje
-                texto = linea
+                texto = línea
                 enviado = @tg.send_message(chat_id: chat_id, text: texto + cargando,
                                            parse_mode: 'html',
                                            disable_web_page_preview: true,
@@ -92,7 +93,7 @@ class Dankie
 
             # Si no, edito el actual
             else
-                texto << linea
+                texto << línea
                 @tg.edit_message_text(chat_id: chat_id, text: texto + cargando,
                                       parse_mode: 'html',
                                       message_id: enviado.message_id,
@@ -100,15 +101,15 @@ class Dankie
                                       disable_notification: true)
             end
 
-            indice += 1
+            índice += 1
         end
     end
 
-    def dame_nismanes(poles)
+    def calcular_total_poles(poles)
         acumulador = 0
-        poles.each do |iterador|
-            acumulador += iterador[1].to_i
+        poles.each do |pole|
+            acumulador += pole[1].to_i
         end
-        acumulador.to_s
+        acumulador
     end
 end
