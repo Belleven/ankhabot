9c9c6efb46e8835182189bedf1abe68c8bea1ec6
galerazo <caleidoscopioperonista@gmail.com>
2019-07-21 20:09:25 -0300

    Saludo a nuevos miembros ya contempla el caso en que el bot esté entre los nuevos usuarios

--- plugins/nuevos_miembro.rb
+++ plugins/nuevos_miembro.rb
@@ -2,11 +2,17 @@ class Dankie
     add_handler NuevosMiembros.new(:nuevos_miembros)
 
     def nuevos_miembros(msj)
-        if msj.new_chat_members.size == 1
-            saludo = ['Bienvenido ', 'Hola ', 'Buenas ', 'Que onda '].sample
+
+    	if msj.new_chat_members.any? { |miembro| miembro.id == @user.id }
+        	nombre_bot = if @user.first_name.nil? then @user.id.to_s else @user.first_name end
+        	saludo = "hyoroskune soy #{nombre_bot} nwn arigato x meterme al grupete"
+	   	    @tg.send_message(chat_id: msj.chat.id, text: saludo) 
+
+        elsif msj.new_chat_members.size == 1
+	    	saludo = ['Bienvenido ', 'Hola ', 'Buenas ', 'Que onda '].sample
             nombre = primer_nombre(msj.new_chat_members[0])
-            extra = [', pasá y sentate al fondo.', ', ponete cómodo.', ', querés bizcochitos?', ', qué te trae por acá?'].sample
-            @tg.send_message(chat_id: msj.chat.id, text: saludo + nombre + extra)
+        	extra = [', pasá y sentate al fondo.', ', ponete cómodo.', ', querés bizcochitos?', ', qué te trae por acá?'].sample
+    	    @tg.send_message(chat_id: msj.chat.id, text: saludo + nombre + extra)   
 
         else
             texto = ['Bienvenidos ', 'Hola ', 'Buenas ', 'Que onda '].sample
