294c7ac2545a389f743afd98f16c495687cff6c2
galerazo <caleidoscopioperonista@gmail.com>
2019-07-23 18:51:59 -0300

    Comando salir ahora solo puede usarse cuando un dev responde un mensaje del bot

--- plugins/salir.rb
+++ plugins/salir.rb
@@ -3,7 +3,12 @@ class Dankie
 
     def salir(msj)
         if validar_desarrollador(msj.from.id, msj.chat.id, msj.message_id) && validar_grupo(msj.chat.type, msj.chat.id, msj.message_id)
-            @tg.leave_chat(chat_id: msj.chat.id)
+            if msj.reply_to_message.nil? || msj.reply_to_message.from.id != @user.id
+                texto_error = 'Si querés que me vaya del grupo, mandá ese comando respondiéndome un mensaje'
+                @tg.send_message(chat_id: msj.chat.id, reply_to_message: msj.message_id, text: texto_error)
+            else
+                @tg.leave_chat(chat_id: msj.chat.id)
+            end
         end
     end
 end
