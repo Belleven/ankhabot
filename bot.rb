15a4e66e0ee0ed8576378d2c31352f4c0de42149
galerazo <caleidoscopioperonista@gmail.com>
2019-08-04 17:09:28 -0300

    Cambio algunas cosillas de bot.rb

--- bot.rb
+++ bot.rb
@@ -1,10 +1,12 @@
+# Datos necesarios para iniciar
 require 'yaml'
+# Esto prepara a Dankie
 require_relative 'lib/dankie'
-Dir[File.dirname(__FILE__) + '/plugins/*.rb'].each { |file| require_relative file } # esta cosa carga todos los plugins del bot
+# Esto carga todos los plugins del bot
+Dir[File.dirname(__FILE__) + '/plugins/*.rb'].each { |file| require_relative file }
 
 config = YAML.load_file(File.join(__dir__, 'config.yml'))
 config.transform_keys!(&:to_sym)
 
 dankie = Dankie.new config
-
 dankie.run
