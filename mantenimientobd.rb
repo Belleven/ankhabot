12c176c489aac0e4b59851cd2bc2b40bdc0b7837
galerazo <caleidoscopioperonista@gmail.com>
2019-10-21 12:21:52 -0300

    Emprolijo script para mantener la base de datos

--- mantenimientobd.rb
+++ mantenimientobd.rb
@@ -2,51 +2,43 @@
 require 'yaml'
 require 'redis'
 
-datazos = YAML.load_file(File.join(__dir__, 'config.yml'))
-datazos.transform_keys!(&:to_sym)
-
-$redis = Redis.new port: datazos[:redis_port], host: datazos[:redis_host], password: datazos[:redis_pass]
-
-def imprimir_todas_las_claves(patron = 'trigger')
-    $redis.keys.each do |clave|
+def imprimir_todas_las_claves(redis, patron = '')
+    redis.keys.each do |clave|
         if clave.include? patron
-
-            if $redis.type(clave) == 'string'
-
-                valor = $redis.get(clave)
-                puts 'CLAVE NORMAL: ' + clave + "\nVALOR: " + valor + "\n\n"
-
-            elsif $redis.type(clave) == 'list'
-
-                valores = $redis.lrange(clave, 0, -1)
-                puts 'LISTA: ' + clave + "\nELEMENTOS: " + valores.to_s + "\n\n"
-
-            elsif $redis.type(clave) == 'set'
-
-                valores = $redis.smembers(clave)
-                puts 'CONJUNTO: ' + clave + "\nELEMENTOS: " + valores.to_s + "\n\n"
-
-            elsif $redis.type(clave) == 'zset'
-
-                valores = $redis.zrevrange(clave, 0, -1, with_scores: true)
-                puts 'CONJUNTO ORDENADO: ' + clave + "\nELEMENTOS: "
+            
+            case redis.type(clave)
+            
+            when 'string'
+                valor = redis.get(clave)
+                puts "CLAVE NORMAL: #{clave}\nVALOR: #{valor}\n\n"
+            
+            when 'list'
+                valores = redis.lrange(clave, 0, -1)
+                puts "LISTA: #{clave}\nELEMENTOS: #{valores}\n\n"
+            
+            when 'set'
+                valores = redis.smembers(clave)
+                puts "CONJUNTO: #{clave}\nELEMENTOS: #{valores}\n\n"
+            
+            when 'zset'
+                valores = redis.zrevrange(clave, 0, -1, with_scores: true)
+                puts "CONJUNTO ORDENADO: #{clave}\nELEMENTOS: "
                 valores.each do |atributo|
-                    puts ' ' + atributo[0].to_s + ': ' + atributo[1].to_s
+                    puts " #{atributo[0]}: #{atributo[1]}"
                 end
                 puts "\n"
-
-            elsif $redis.type(clave) == 'hash'
-
-                valores = $redis.hgetall(clave)
-                puts 'HASH: ' + clave + "\nELEMENTOS: "
+            
+            when 'hash'
+                valores = redis.hgetall(clave)
+                puts "HASH: #{clave}\nELEMENTOS: "
                 valores.each do |atributo|
-                    puts ' ' + atributo[0].to_s + ': ' + atributo[1].to_s
+                    puts " #{atributo[0]}: #{atributo[1]}"
                 end
                 puts "\n"
-
-            elsif $redis.type(clave) == 'stream'
-
-                puts 'STREAM: ' + clave + "\nVALOR: ¿QUÉ MIERDA ES ESTO FLACO? USÁ CLAVES NORMALES\n\n"
+            
+            when 'stream'
+                puts "STREAM: #{clave}\n"\
+                     "VALOR: ¿QUÉ MIERDA ES ESTO FLACO? USÁ CLAVES NORMALES\n\n"
 
             end
 
@@ -55,20 +47,27 @@ def imprimir_todas_las_claves(patron = 'trigger')
 end
 
 def modificar_base
+    datazos = YAML.load_file(File.join(__dir__, 'config.yml'))
+    datazos.transform_keys!(&:to_sym)
+    
+    redis = Redis.new(port: datazos[:redis_port],
+                       host: datazos[:redis_host],
+                       password: datazos[:redis_pass])
+
     puts "\n"
     puts '-' * 30
     puts 'ANTES DE CAMBIAR LA BBDD'
     puts '-' * 30
     puts "\n"
 
-    imprimir_todas_las_claves
+    imprimir_todas_las_claves redis
 
     puts '-' * 30
     puts 'CAMBIANDO LA BBDD'
     puts '-' * 30
 
     # Acá meter funciones para modificar la BD, NO OLVIDARSE de borrarlos después
-    # Por ejemplo: $redis.del("agregar:5")
+    # Por ejemplo: redis.del("agregar:5")
 
     puts 'CAMBIADA'
     puts '-' * 30
@@ -76,7 +75,7 @@ def modificar_base
     puts '-' * 30
     puts "\n"
 
-    imprimir_todas_las_claves
+    imprimir_todas_las_claves redis
 
     puts '-' * 30
     puts 'FIN'
