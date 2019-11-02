11b60e2f3f26b1a4d4ce7ada555055344f0bd8f1
Lucas F. Ovalle Olivares <luke.ovalle@gmail.com>
2019-11-02 01:04:25 -0300

    x2: restaurado a como debe funcionar

--- módulos/x2.rb
+++ módulos/x2.rb
@@ -1,12 +1,11 @@
 class Dankie
-    add_handler Handler::Mensaje.new(:x2)
+    add_handler Handler::Mensaje.new(:x2, tipos: [:text])
 
     def x2(msj)
-        return unless (mensaje = msj.text)
-
         texto = msj&.reply_to_message&.text || msj&.reply_to_message&.caption
         return unless texto
 
+        mensaje = msj.text.split.first
         return unless /\A[xX*](\d+)\z/.match? mensaje
 
         n = mensaje[1..].to_i
@@ -16,10 +15,14 @@ class Dankie
 
         n = (4096 / texto.length) + 1 if (texto.length * n - 1) > 4096
 
-        texto *= n
-        cansado = '... ya fue, me cansé.'
-        if texto.length >= 4096 - cansado.length
-            texto = texto[0..(4096 - cansado.length - 1)] + cansado
+        if n.positive?
+            texto *= n
+            cansado = '... ya fue, me cansé.'
+            if texto.length >= 4096 - cansado.length
+                texto = texto[0..(4096 - cansado.length - 1)] + cansado
+            end
+        else
+            texto = '""'
         end
 
         resp = @tg.send_message(chat_id: msj.chat.id, text: texto)
