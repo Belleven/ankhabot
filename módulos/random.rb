0725b20376f48515e812c97061142a47b083efdb
Chiro1975 <tach.365@gmail.com>
2020-08-24 19:30:55 -0300

    Agregado mas opciones de dados en tirada

--- módulos/random.rb
+++ módulos/random.rb
@@ -9,7 +9,7 @@ class Dankie
 		
 #Comprueba que sea un texto valido
 	def comprobar_dado(texto)
-		return texto.match(/[1-9]*d(4|6|8|10|12|20)(\+[1-9])?/)
+		return texto.match(/[1-9]*d([1-9][0-9][0-9])(\+[1-9])?/)
 	end	
 
 	def enviar_mensaje_error(error_code)
@@ -17,6 +17,8 @@ class Dankie
 			dado_texto = "Mira kpo/a, vos te pensas que te voy a tirar tantas veces el dado? Hasta 666 te lo hago sin problemas, pero ubicate"
 		elsif error_code == "modificador"
 			dado_texto = "Por que queres un modificador tan alto??? Hasta 999 te acepto pero sino es mucho trabajo"
+		elsif error_code == "cantidad_caras"
+			dado_texto = "Vos te pensas que me alcanza la plata para tener dados tan altos??? El maximo que tengo es de 999 caras"		
 		end	
 		return dado_texto
 	end		
@@ -51,10 +53,15 @@ class Dankie
 	def get_cantidad_caras(texto)
 		$indice = $indice + 1
 		cantidad_caras = ""
-		while texto[$indice] != "+" and texto[$indice] != "." do
+		contador = 0
+		while texto[$indice] != "+" and texto[$indice] != "." and contador < 4 do
 			cantidad_caras = cantidad_caras + texto[$indice]
+			contador = contador + 1
 			$indice = $indice + 1
 		end
+		if contador == 4
+			cantidad_caras = "-1"
+		end	
 		return cantidad_caras	
 	end		
 
@@ -105,29 +112,31 @@ class Dankie
 			#Si es -1, indica que se quizo tirar mas veces de lo permitido
 			if cantidad_dados == "-1"
 				error_code = "caras"
-				error_flag = 1
 				dado_texto = enviar_mensaje_error(error_code)
 			else	
 
 				number_string  = get_cantidad_caras(texto)
-				modificador = ""
-				#El texto puede no tener modificadores, por eso hay que comprobarlo
-				if texto[$indice] == "+"
-					modificador    = get_modificador(texto)
-				end	
-
-				#Si se produce un modificador mas alto de lo aceptado
-				if (modificador == "-1")
-					error_code = "modificador"
-					error_flag = 1
+				if number_string == "-1"
+					error_code = "cantidad_caras"
 					dado_texto = enviar_mensaje_error(error_code)
-
 				else
-					dado_completo = dado_completo_string(texto)
-					dado_texto = conseguir_valor_dado(number_string, cantidad_dados,  modificador)
-					dado_texto = dado_completo + "\n" + dado_texto
-					
-				end
+					modificador = ""
+					#El texto puede no tener modificadores, por eso hay que comprobarlo
+					if texto[$indice] == "+"
+						modificador    = get_modificador(texto)
+					end	
+
+					#Si se produce un modificador mas alto de lo aceptado
+					if (modificador == "-1")
+						error_code = "modificador"
+						dado_texto = enviar_mensaje_error(error_code)
+					else
+						dado_completo = dado_completo_string(texto)
+						dado_texto = conseguir_valor_dado(number_string, cantidad_dados,  modificador)
+						dado_texto = dado_completo + "\n" + dado_texto
+						
+					end
+				end	
 			end
 		else
 			dado_texto = "Kpo, no sabes jugar D&D? El comando es /tirada <NdR+M> donde N es el numero de tiradas y R son las caras usadas que solo pueden ser 4, 6, 8, 10, 12, 20. Si solo esta el rango, devuelve una sola tirada. M es un modificador que aumenta el valor de tus tiros, puede no estar"	
