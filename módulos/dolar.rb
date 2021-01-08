187e5443aebe6b0677478829a14721ce625d63f4
Chiro1975 <tach.365@gmail.com>
2021-01-08 14:14:41 -0300

    Cambiado un poco dolar

--- módulos/dolar.rb
+++ módulos/dolar.rb
@@ -7,10 +7,12 @@ class Dankie
         permitir_params: false
     )
     def dolar(msj)
-        dolar = Dolar.new
-        texto = dolar.scrap
+        @dolar ||= Dolar.new
+
+        texto = @dolar.scrap
         @tg.send_message(chat_id: msj.chat.id, parse_mode: :html,
                          text: texto,
+                         descripción: 'Te muestra el precio del dolar',
                          disable_web_page_preview: true,
                          disable_notification: true)
     end
@@ -18,52 +20,50 @@ class Dankie
     class Dolar
         def initialize
             @doc = Nokogiri::HTML(URI.open('https://www.dolarhoy.com/'))
-            # puts(@doc)
+            @path_padre = "//div[contains(@class,'pill pill-coti')]".freeze
+            @path_dolar = '//h4/a'.freeze
+            @path_valores = "//div[contains(@class, 'col-6 text-center')]".freeze
+            @path_solidario = "//div[contains(@class, 'col-12 text-center')]".freeze
+            @path_actualizacion = "//div[contains(@class,'col-7 text-right')]".freeze
         end
 
         def scrap
             # Consigo la ruta general donde se encuentran los demas valores
-            paginas = @doc.xpath("//div[contains(@class,'container body-content')]/
-                div[contains(@class,'container')]/div[contains(@class,'row')]/
-                div[contains(@class,'col-md-8')]/div[contains(@class,'row')]/
-                div[contains(@class,'col-12 col-lg-6')]
-                /div[contains(@class,'pill pill-coti')]")
+            paginas = @doc.xpath(@path_padre)
 
             # Agarro todos los dolares posibles
-            dolar = paginas.xpath('//h4/a').collect { |node| node.text.strip }
+            dolar = conseguir_elementos(paginas, @path_dolar)
 
             # Cuento su valor
             # Los valores de compra y venta vienen juntos, por lo cual los
             # que separar despues
-            valor = paginas.xpath("//div[contains(@class,'row')]/
-                div[contains(@class, 'col-6 text-center')]").collect do |node|
-                node.text.strip
-            end
+            valor = conseguir_elementos(paginas, @path_valores)
 
             # Como soy solidario, pago mas
-            valor_solidario = paginas.xpath("//div[contains(@class,'row')]/
-                div[contains(@class, 'col-12 text-center')]").collect do |node|
-                node.text.strip
-            end
+            valor_solidario = conseguir_elementos(paginas, @path_solidario)
+
             # Agrupo los valores de a dos, compra y venta juntos
             compra_venta = agrupar_compra_venta(valor, 2)
+
             # Tambien para el solidario
             compra_venta_total = agrupar_compra_venta(valor_solidario, 1,
                                                       compra_venta: compra_venta)
 
             # Leo cuando fue la ultima vez que se actualizo el valor
-            actualizacion = paginas.xpath("//div[contains(@class,'foot')]/
-                div[contains(@class,'row')]/
-                div[contains(@class,'col-7 text-right')]").first.text.strip
+            actualización = paginas.xpath(@path_actualizacion).first.text.strip
 
             # Armo el texto con toda la informacion
             texto = mezclar_dolar_valor(dolar, compra_venta_total)
-            texto << "\n" << actualizacion
+            texto << "\n" << actualización
 
             texto
         end
 
         private
+
+        def conseguir_elementos(paginas, path)
+            paginas.xpath(path).collect { |node| node.text.strip }
+        end
 
         def agrupar_compra_venta(valores, contador_limite, compra_venta: [])
             texto = ''
